79171c492a1772ce2671b1ea4f478c73bd3daddc
박정환/서비스프로토타입개발팀 <jeonghwan.dev@gmail.com>
2025-05-11 15:20:35 +0900

    feat: 아티클 상세 페이지 개발

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
 import type { NextConfig } from 'next';
 
+import createMDX from '@next/mdx';
+
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
@@ -9,6 +11,7 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/,
@@ -19,4 +22,6 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+const withMDX = createMDX({});
+
+export default withMDX(nextConfig);
