7c03b6f307536135822b8c2d033b045b7fe9242c
박정환 <jeonghwan.dev@gmail.com>
2025-05-14 07:47:28 +0900

    feat: Container 스타일 수정 및 불필요한 클래스 제거 (#39)

--- src/components/layouts/footer.tsx
+++ src/components/layouts/footer.tsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <footer className="w-full border-t-1 border-dashed">
-      <div className="container-padding container mx-auto border-x-1 border-dashed py-4">
+      <div className="container mx-auto border-x-1 border-dashed py-4">
         <span className="text-muted-foreground text-sm">
           Copyright © Park Jeong Hwan. All Rights Reserved.
         </span>
