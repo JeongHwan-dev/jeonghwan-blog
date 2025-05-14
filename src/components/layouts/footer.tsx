bd1e46f848026a202bd09f6405889236cdb43edf
박정환 <jeonghwan.dev@gmail.com>
2025-05-14 23:54:35 +0900

    feat: 레이아웃 UI 스타일 수정 (#40)

--- src/components/layouts/footer.tsx
+++ src/components/layouts/footer.tsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
-    <footer className="w-full border-t-1 border-dashed">
-      <div className="container mx-auto border-x-1 border-dashed py-4">
+    <footer className="h-[var(--footer-height)] w-full border-t-1 border-dashed">
+      <div className="container mx-auto flex h-full items-center border-x-1 border-dashed">
         <span className="text-muted-foreground text-sm">
           Copyright © Park Jeong Hwan. All Rights Reserved.
         </span>
