f9c926c1733670c807b4e307f96a797985699e95
박정환/서비스프로토타입개발팀 <jeonghwan.dev@gmail.com>
2025-05-18 22:00:49 +0900

    feat: Footer 컴포넌트 문구 표기 개선

--- src/components/layouts/footer.tsx
+++ src/components/layouts/footer.tsx
@@ -1,9 +1,11 @@
+import { PROFILE } from '@/constants';
+
 function Footer() {
   return (
     <footer className="h-[var(--footer-height)] w-full border-t-1 border-dashed">
       <div className="container flex h-full items-center">
         <span className="text-muted-foreground text-sm">
-          Copyright © Park Jeong Hwan. All Rights Reserved.
+          {`Copyright © ${PROFILE.englishName}. All Rights Reserved.`}
         </span>
       </div>
     </footer>
