79541c884efbcf525f7fc22ee57d982de5b3a29a
박정환/서비스프로토타입개발팀 <jeonghwan.dev@gmail.com>
2025-05-18 20:21:59 +0900

    feat: 프로필 카드 컴포넌트 추가 및 홈 페이지 레이아웃 수정

---
+++ src/constants/profile.ts
@@ -0,0 +1,10 @@
+export const PROFILE = {
+  company: 'LG U+',
+  email: 'jeonghwan.dev@gmail.com',
+  englishName: 'Park Jeong Hwan',
+  gitHubUrl: 'https://github.com/JeongHwan-dev',
+  job: 'Frontend Developer',
+  koreanName: '박정환',
+  linkedInUrl: 'https://www.linkedin.com/in/jeonghwan-dev',
+  profileImageUrl: '/images/profile/img-profile.jpg',
+} as const;
