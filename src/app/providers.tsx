4e4acb71520ae437fc72bbe5ebbc550491c99fe1
박정환 <jeonghwan.dev@gmail.com>
2025-06-13 23:01:55 +0900

    chore: ReactQueryDevtools 추가 (#51)

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { type PropsWithChildren, useState } from 'react';
 
 function Providers({ children }: PropsWithChildren) {
@@ -15,7 +16,12 @@ function Providers({ children }: PropsWithChildren) {
       }),
   );
 
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
 
 export { Providers };
