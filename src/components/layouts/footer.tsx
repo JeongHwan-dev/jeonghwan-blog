402f58c99aa83ade5e8a80e46fb2254387274687
박정환 <jeonghwan.dev@gmail.com>
2025-06-14 23:09:44 +0900

    refactor: 유틸리티 클래스 사용 코드 개선 (#69)

--- src/components/layouts/footer.tsx
+++ src/components/layouts/footer.tsx
@@ -2,7 +2,7 @@ import { PROFILE } from '@/constants';
 
 function Footer() {
   return (
-    <footer className="h-[var(--footer-height)] w-full border-t-1 border-dashed">
+    <footer className="h-[var(--footer-height)] w-full border-t border-dashed">
       <div className="container flex h-full items-center">
         <span className="text-muted-foreground text-sm">
           {`© 2025. ${PROFILE.englishName}. All rights reserved.`}
