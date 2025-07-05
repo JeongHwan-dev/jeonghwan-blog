c77e3a38bfbff63e7698f4851ea07e55bb1b9b36
박정환/서비스프로토타입개발팀 <jeonghwan.dev@gmail.com>
2025-07-06 00:19:19 +0900

    docs: 유틸리티 함수의 JSDoc 주석을 한국어로 변경

--- src/lib/utils/tailwind-utils.ts
+++ src/lib/utils/tailwind-utils.ts
@@ -2,9 +2,9 @@ import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
 /**
- * Combines and merges Tailwind CSS class names using clsx and tailwind-merge
- * @param inputs Class values to combine (strings, objects, arrays, etc.)
- * @returns Merged and deduplicated class name string
+ * clsx와 tailwind-merge를 사용하여 Tailwind CSS 클래스명을 결합하고 병합합니다
+ * @param inputs 결합할 클래스 값들 (문자열, 객체, 배열 등)
+ * @returns 병합되고 중복이 제거된 클래스명 문자열
  */
 export function cn(...inputs: ClassValue[]): string {
   return twMerge(clsx(inputs));
