d078ae4fd5caefd903d3390afcc414a30bf95d02
박정환/서비스프로토타입개발팀 <jeonghwan.dev@gmail.com>
2025-07-22 22:20:10 +0900

    refactor: 메타데이터 설정 코드 구조 개선

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-import type { Metadata } from 'next';
 import type { ReactNode } from 'react';
 
 import { Analytics } from '@vercel/analytics/react';
@@ -6,8 +5,8 @@ import { SpeedInsights } from '@vercel/speed-insights/next';
 import localFont from 'next/font/local';
 
 import { Footer, Header, ScrollFloatingActionButtonGroup } from '@/components';
-import { PROFILE } from '@/constants';
 
+import { metadata } from './_constants';
 import { Providers } from './providers';
 import './globals.css';
 
@@ -23,61 +22,7 @@ const pretendard = localFont({
   weight: '45 920',
 });
 
-const BLOG_TITLE = `Jeonghwan's Blog`;
-
-export const metadata: Metadata = {
-  alternates: {
-    canonical: '/',
-  },
-  authors: [
-    {
-      name: PROFILE.koreanName,
-      url: PROFILE.linkedInUrl,
-    },
-  ],
-  creator: PROFILE.koreanName,
-  description:
-    '웹 개발 지식과 실무 경험을 나누는 테크 블로그입니다. 개발 과정에서 얻은 인사이트와 최신 웹 기술 트렌드 등을 공유합니다.',
-  formatDetection: {
-    address: false,
-    email: false,
-    telephone: false,
-  },
-  icons: {
-    icon: '/favicon.ico',
-  },
-  keywords: [
-    '개발 블로그',
-    '프론트엔드',
-    '개발자',
-    '웹 개발',
-    'Tech Blog',
-    'Frontend',
-    'Developer',
-    'Web Development',
-  ],
-  metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL as string),
-  openGraph: {
-    description:
-      '웹 개발 지식과 실무 경험을 나누는 테크 블로그입니다. 개발 과정에서 얻은 인사이트와 최신 웹 기술 트렌드 등을 공유합니다.',
-    images: [
-      {
-        alt: BLOG_TITLE,
-        height: 675,
-        url: '/images/img-default-og.png',
-        width: 1200,
-      },
-    ],
-    title: BLOG_TITLE,
-    type: 'website',
-    url: '/',
-  },
-  publisher: PROFILE.koreanName,
-  title: {
-    default: BLOG_TITLE,
-    template: `%s | ${BLOG_TITLE}`,
-  },
-};
+export { metadata };
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
