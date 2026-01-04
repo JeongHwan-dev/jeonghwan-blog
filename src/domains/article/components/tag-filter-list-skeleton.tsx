c3296ca609372c9c76840703d85093c2c1430926
박정환/서비스프로토타입개발팀 <jeonghwan.dev@gmail.com>
2026-01-04 23:42:08 +0900

    feat(article): TagFilterListSkeleton의 아이템 개수 변경

--- src/domains/article/components/tag-filter-list-skeleton.tsx
+++ src/domains/article/components/tag-filter-list-skeleton.tsx
@@ -2,7 +2,7 @@ import { Repeat, Skeleton } from '@/shared/components';
 
 function TagFilterListSkeleton() {
   return (
-    <Repeat as="ul" times={5} className="flex flex-col gap-3">
+    <Repeat as="ul" times={8} className="flex flex-col gap-3">
       <li>
         <Skeleton className="h-9 w-full" />
       </li>
