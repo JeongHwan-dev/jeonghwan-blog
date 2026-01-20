f08f9f6084ae3124364f27e9d1d31b87fe7e2ad2
박정환/서비스프로토타입개발팀 <jeonghwan.dev@gmail.com>
2026-01-20 23:06:39 +0900

    chore(github): generate-label 스크립트의 import 경로 오타 수정

--- .github/scripts/generate-labels.mjs
+++ .github/scripts/generate-labels.mjs
@@ -6,9 +6,9 @@
 
 import fs from 'node:fs';
 import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 import { LABELS } from '../labels/labels.config.mjs';
-import { fileURLToPath } from 'node: url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
